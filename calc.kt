6099704b43955b6753e121d26d062ef39b432bf5
Pierre <65714920+Sylvorus@users.noreply.github.com>
2023-09-17 02:09:01 +0100

    Create calc.kt

---
+++ calc.kt
@@ -0,0 +1,27 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val reader = Scanner(System.`in`)
+    print("enter two numbers: ")
+
+    val first = reader.nextDouble()
+    val second = reader.nextDouble()
+
+    print("enter operation (+,-,*,/)")
+    val operator = reader.next()[0]
+
+    val result: Double
+
+    when (operator) {
+        '+' -> result = first + second
+        '-' -> result = first - second
+        '*' -> result = first * second
+        '/' -> result = first / second
+        else -> {
+            System.out.printf("error")
+            return
+        }
+    }
+
+    System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result)
+}
